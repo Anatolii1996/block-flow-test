a002caad25013376595f3ac87053a950e992bd93
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-07-05 20:17:15 +0300

    fixet bug with setting

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ let asside_buttons = document.querySelectorAll(".aside_button");
 for (const button of asside_buttons) {
     button.addEventListener("mouseover", () => {
         button.classList.add("view");
-        let icon = button.querySelector("svg  >  g > path");
-        icon.setAttribute('stroke', '#1b1b1b');
+        let icons = button.querySelectorAll("svg  >  g > path");
+        for (const icon of icons) {
+            icon.setAttribute('stroke', '#1b1b1b');
+        }
+
 
     })
     button.addEventListener("mouseout", () => {
         button.classList.remove("view");
-        let icon = button.querySelector("svg  >  g > path");
-        icon.setAttribute('stroke', '#667085');
+        let icons = button.querySelectorAll("svg  >  g > path");
+        for (const icon of icons) {
+            icon.setAttribute('stroke', '#667085');
+        }
+
     })
 }
 
-// let footer_botton=document.querySelector(".footer_button");
-// footer_botton.addEventListener("mouseover", () => {
-//     button.classList.add("view");
-// });
 
-footer_botton.addEventListener("mouseout", () => {
-    button.classList.remove("view");
-});
